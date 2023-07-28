98df0ddaf6a8d822fd1ef5445f354032ead80444
iam3dski <123909952+iam3dski@users.noreply.github.com>
2023-07-28 16:18:36 -0400

    update connection.js for Heroku

--- config/connection.js
+++ config/connection.js
@@ -3,15 +3,25 @@ require('dotenv').config();
 
 let sequelize;
 
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
+if (process.env.DATABASE_URL) {
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false // For self-signed certificates
+      }
+    }
+  });
 } else {
+  // Your local development configuration here
   sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-      host: 'localhost',
+      host: 'localhost', // Update this to your local MySQL host if needed
       dialect: 'mysql',
       port: 3306
     }
