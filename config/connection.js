8fb0657bbf3a98c58004c7183cf0e98177c0b7fe
iam3dski <123909952+iam3dski@users.noreply.github.com>
2023-07-28 16:41:43 -0400

    update connection.js for Heroku again and again

--- config/connection.js
+++ config/connection.js
@@ -15,17 +15,12 @@ if (process.env.DATABASE_URL) {
     },
   });
 } else {
-  // Local development configuration
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306,
-    }
-  );
+  // Your local development configuration here
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306,
+  });
 }
 
 module.exports = sequelize;
